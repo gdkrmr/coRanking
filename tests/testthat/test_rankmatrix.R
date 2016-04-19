41c9d74694164930e879f2e8ff96ca2485a1e6c4
gdkrmr <gkraemer@bgc-jena.mpg.de>
2016-04-19 11:29:03 +0200

    fixed some testing errors

--- tests/testthat/test_rankmatrix.R
+++ tests/testthat/test_rankmatrix.R
@@ -13,61 +13,34 @@ m2 <- matrix(c(2., 3., 4., 5.,
 context('rankmatrix')
 
 test_that('output dimensions', {
-    expect_equal(nrow(rankmatrix(df1, df2, use = 'C')),
+    expect_equal(nrow(rankmatrix(df1, use = 'C')),
                  nrow(df1))
-    expect_equal(nrow(rankmatrix(df1, df2, use = 'R')),
+    expect_equal(nrow(rankmatrix(df1, use = 'R')),
                  nrow(df1))
-    expect_equal(nrow(rankmatrix(m1, m2, use = 'C')),
-                 nrow(df2))
-    expect_equal(nrow(rankmatrix(m1, m2, use = 'R')),
-                 nrow(df2))
-    expect_equal(nrow(rankmatrix(df1, m2, use = 'R')),
-                 nrow(df2))
+    expect_equal(nrow(rankmatrix(m1, use = 'C')),
+                 nrow(m1))
+    expect_equal(nrow(rankmatrix(m1, use = 'R')),
+                 nrow(m1))
+    expect_equal(nrow(rankmatrix(m2, use = 'R')),
+                 nrow(m2))
 })
 
 test_that('data.frame and matrix', {
-    expect_equal(rankmatrix(df1, df2, use = 'C'),
-                 rankmatrix(m1, m2,   use = 'C'))
-    expect_equal(rankmatrix(df1, df2, use = 'R'),
-                 rankmatrix(m1, m2,   use = 'R'))
+    expect_equal(rankmatrix(df1, use = 'C'),
+                 rankmatrix(m1,  use = 'C'))
+    expect_equal(rankmatrix(df1, use = 'R'),
+                 rankmatrix(m1,  use = 'R'))
 })
-
-test_that('equal data results in eye', {
-    expect_equal(rankmatrix(df2, df2, use = 'C'),
-                 diag(nrow(df2) - 1))
-    expect_equal(rankmatrix(df2, df2,  use = 'R'),
-                 diag(nrow(df2) - 1))
-    expect_equal(rankmatrix(df1, df1, use = 'C'),
-                 diag(nrow(df1) - 1))
-    expect_equal(rankmatrix(df1, df1,  use = 'R'),
-                 diag(nrow(df1 - 1)))
-    expect_equal(rankmatrix(m2, m2, use = 'C'),
-                 diag(nrow(m2) - 1))
-    expect_equal(rankmatrix(m2, m2,  use = 'R'),
-                 diag(nrow(m2) - 1))
-    expect_equal(rankmatrix(m1, m1, use = 'C'),
-                 diag(nrow(m1) - 1))
-    expect_equal(rankmatrix(m1, m1,  use = 'R'),
-                 diag(nrow(m2) - 1))
-})
-
-test_that('changing arguments transposes results', {
-    expect_equal(rankmatrix(df1, df2, use = 'C'),
-                 rankmatrix(df2, df1, use = 'C'))
-    expect_equal(rankmatrix(m2, m1, use = 'R'),
-                 rankmatrix(m1, m2, use = 'R'))
-})
-
 
 test_that('C and R backend equal', {
-    expect_equal(rankmatrix(df1, df2, use = 'C'),
-                 rankmatrix(df1, df2, use = 'R'))
-    expect_equal(rankmatrix(df2, m1, use = 'C'),
-                 rankmatrix(df2, m1, use = 'R'))
-    expect_equal(rankmatrix(m1, m2, use = 'C'),
-                 rankmatrix(m1, m2, use = 'R'))
-    expect_equal(rankmatrix(m2, m1, use = 'C'),
-                 rankmatrix(m2, m1, use = 'R'))
+    expect_equal(rankmatrix(df1, use = 'C'),
+                 rankmatrix(df1, use = 'R'))
+    expect_equal(rankmatrix(df2, use = 'C'),
+                 rankmatrix(df2, use = 'R'))
+    expect_equal(rankmatrix(m1, use = 'C'),
+                 rankmatrix(m1, use = 'R'))
+    expect_equal(rankmatrix(m2, use = 'C'),
+                 rankmatrix(m2, use = 'R'))
 })
 
 df3 <- matrix(letters[1:8], ncol = 2)
